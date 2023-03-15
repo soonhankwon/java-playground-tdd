d7e7757eaa621a5bc99eb0725461a81c0dce42e8
soonable@naver.com <soonable@naver.com>
2023-03-15 18:04:11 +0900

    refactor : 모든 원시값과 문자열을 포장

--- src/main/java/com/example/javaplaygroundtdd/racing/domain/Car.java
+++ src/main/java/com/example/javaplaygroundtdd/racing/domain/Car.java
@@ -6,27 +6,22 @@ public class Car {
     private static final int FORWARD_NUM = 4;
     private static final int MAX_BOUND = 10;
 
-    private final String name;
+    private final Name name;
     private int position = 0;
+    private Position position2;
 
     public Car(final String name) {
-        if (StringUtils.isBlank(name)) {
-            throw new IllegalArgumentException("자동차 이름은 값이 존재해야 합니다.");
-        }
-        this.name = name.trim();
+        this.name = new Name(name);
+        this.position2 = new Position(0);
     }
 
     public int getPosition() {
         return position;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void move(MovingStrategy movingStrategy) {
         if(movingStrategy.movable())
-            this.position++;
+            position2 = position2.move();
     }
 
     public void move(int randomNo) {
---
+++ src/main/java/com/example/javaplaygroundtdd/racing/domain/Position.java
@@ -0,0 +1,44 @@
+package com.example.javaplaygroundtdd.racing.domain;
+
+import java.util.Objects;
+
+public class Position {
+    private int position;
+
+    public Position() {
+        this(0);
+    }
+
+    public Position(int position) {
+        if(position < 0) {
+            throw new IllegalArgumentException("position은 음수 값을 가질 수 없습니다.");
+        }
+        this.position = position;
+    }
+
+    public Position move() {
+        position = position + 1;
+        return this;
+    }
+
+    public void move3() {
+        position = position + 1;
+    }
+
+    public Position move2() {
+        return new Position(position + 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
